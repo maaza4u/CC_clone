7914c74226347aa2ade3aafe2a437795fd0d466b
maaza4u <98598449+maaza4u@users.noreply.github.com>
2024-05-22 09:55:27 +0530

    Update Administration.js

--- src/Components/Administration.js
+++ src/Components/Administration.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, List, ListItem, ListItemText, Typography } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, List } from '@mui/material';
 
 const Administration = () => {
   const [selectedItem, setSelectedItem] = useState(null);
